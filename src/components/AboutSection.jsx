ff21436e2f4f596c7ab33b156bc15444aae995b0
KabTheProgrammer <ikabiru038@gmai.com>
2025-05-09 21:17:12 +0000

    add cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,68 +1,99 @@
-import { Briefcase, Code, User } from 'lucide-react'
-import React from 'react'
+import { Briefcase, Code, User } from "lucide-react";
+import React from "react";
 
 const AboutSection = () => {
   return (
-    <section id='about' className='py-24 px-4 relative'>
-        <div className='container mx-auto max-w-5xl'>
-            <h2 className='text-3xl md:text-4xl font-bold mb-12 text-center'>
-                About <span className='text-primary'>Me</span>
-            </h2>
+    <section id="about" className="py-24 px-4 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary">Me</span>
+        </h2>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-12 items-center'>
-                <div className='space-y-6'>
-                    <h3 className='text-2xl font-semibold'>Passionate Web Developer & Tech Creator</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Passionate Web Developer & Tech Creator
+            </h3>
 
-                    <p className='text-muted-foreground'>With over 3 years of experience as a full-stack developer, I specialize in building web applications using React and Node.js. I enjoy creating clean, responsive interfaces that deliver smooth user experiences, while ensuring performance and maintainability.</p>
+            <p className="text-muted-foreground">
+              With over 3 years of experience as a full-stack developer, I
+              specialize in building web applications using React and Node.js. I
+              enjoy creating clean, responsive interfaces that deliver smooth
+              user experiences, while ensuring performance and maintainability.
+            </p>
 
-                    <p className='text-muted-foreground'>On the backend, I develop efficient APIs and scalable server-side logic using Node.js and Express, often working with databases like MongoDB. I'm passionate about turning ideas into functional products and continuously improving my skills to stay ahead in the tech world.</p>
+            <p className="text-muted-foreground">
+              On the backend, I develop efficient APIs and scalable server-side
+              logic using Node.js and Express, often working with databases like
+              MongoDB. I'm passionate about turning ideas into functional
+              products and continuously improving my skills to stay ahead in the
+              tech world.
+            </p>
 
-                    <div className='flex flex-col sm:flex-row gap-4 pt-4 justify-center'>
-                        <a href="#contact" className='cosmic-button'> Get In Touch</a>
-                        <a href="" className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300'> Dowload CV</a>
-
-                    </div>
-                </div>
-
-                <div className='grid grid-cols-1 gap-6'>
-                    <div className='gradient-border p-6 card-hover'>
-                        <div className='flex items-start gap-4'>
-                            <div className='p-3 rounded-full bg-primary/10'>
-                            <Code className='h-6 w-6 text-primary'/>
-                            </div>
-                            <div className='text-left'>
-                                <h4 className='font-semibold text-lg'> Web Development</h4>
-                                <p className='text-muted-foreground'>Creating responsive websites and web applications with modern frameworks.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='gradient-border p-6 card-hover'>
-                    <div className='flex items-start gap-4'>
-                            <div className='p-3 rounded-full bg-primary/10'>
-                            <User className='h-6 w-6 text-primary'/>
-                            </div>
-                            <div className='text-left'>
-                                <h4 className='font-semibold text-lg'> UI/UX Design</h4>
-                                <p className='text-muted-foreground'>Designing intuitive user interfaces and seamless user experiences.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='gradient-border p-6 card-hover'>
-                    <div className='flex items-start gap-4'>
-                            <div className='p-3 rounded-full bg-primary/10'>
-                            <Briefcase className='h-6 w-6 text-primary'/>
-                            </div>
-                            <div className='text-left'>
-                                <h4 className='font-semibold text-lg'> Project Manager</h4>
-                                <p className='text-muted-foreground'>Leading projects from conception to completion with agile methologies.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                {" "}
+                Get In Touch
+              </a>
+              <a
+                href="/Ibrahim Kabiru.docx"
+                download
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                {" "}
+                Dowload CV
+              </a>
             </div>
-        </div>
-    </section>
-  )
-}
+          </div>
 
-export default AboutSection
+          <div className="grid grid-cols-1 gap-6">
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Code className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg"> Web Development</h4>
+                  <p className="text-muted-foreground">
+                    Creating responsive websites and web applications with
+                    modern frameworks.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <User className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg"> UI/UX Design</h4>
+                  <p className="text-muted-foreground">
+                    Designing intuitive user interfaces and seamless user
+                    experiences.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Briefcase className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg"> Project Manager</h4>
+                  <p className="text-muted-foreground">
+                    Leading projects from conception to completion with agile
+                    methologies.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
